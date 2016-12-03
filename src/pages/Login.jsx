1cbfd1663586031064516400a36c06cfb3bf5ec9
Harka Lopchan <hlopchan@mblm.com>
2016-12-03 23:03:10 +0400

    updated css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,7 @@ class Login extends Component {
   componentDidMount(){
     loadCSS('assets/pages/css/lock.min.css');
     loadCSS('assets/pages/css/login.min.css');
-    loadCSS('assets/layouts/global/css/main.css');
+    loadCSS('assets/layouts/global/main.css');
     loadCSS('assets/css/style.css');
   }
   render(){
